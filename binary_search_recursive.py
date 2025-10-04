308b265b29e7917396357de8bdba67fea63da745
WalmirFerJr <walmir.jr.2004@usp.br>
2025-10-04 01:02:19 -0300

    Busca binária recursiva

--- binary_search_recursive.py
+++ binary_search_recursive.py
@@ -1,9 +1,19 @@
 def binarySearch(array_test, left, right, key):
     
-    while left <= right:
-        mid = (left + right) / 2
+    mid = (left + right) / 2
+    mid = int(mid)
 
-        if array_test(mid) == key:
-            return array_test(mid)
+    if array_test[mid] == key:
+        return mid
+    elif left >= right:
+        return -1
+    elif key > array_test[mid]:
+        return binarySearch(array_test, mid + 1, right, key)
+    else:
+        return binarySearch(array_test, left, mid - 1, key)
+    
+array_test = [10, 20, 30, 40, 50]
+size_array = len(array_test)
+
+print(binarySearch(array_test, 0, size_array - 1, 30))
         
-        elif:
